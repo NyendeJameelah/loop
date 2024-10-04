61bec24a4698701b5a807b48f9a34f24fac13314
NyendeJameelah <2300716399@mubs.ac.ug>
2024-10-04 16:16:19 +0300

    Add files via upload

---
+++ loop/loop/src/loop/Loops.java
@@ -0,0 +1,19 @@
+package loop;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		while(i<=30);
+		{
+			System.out.println(i);
+			i=i+1;
+			if(i<=30); {
+				System.out.println("correct");
+			}
+		}
+		System.out.println("valid");
+	}
+
+}
